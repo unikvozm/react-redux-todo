a3baba2bdbec32e66059040788032b3b072c5b9a
Natalia Volkova <Natalia.volkofff@gmail.com>
2020-04-23 12:00:37 +0300

    add materialize.css and todos view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Todos } from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy something" },
+      { id: 1, content: "learn something" },
+    ],
+  };
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Todos = ({ todos }) => {
+  const todoList = todos.length ? (
+    todos.map((todo) => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span>{todo.content}</span>
+        </div>
+      );
+    })
+  ) : (
+    <p className="center">You have no todo's left</p>
+  );
+  return <div className="todos collection">{todoList}</div>;
+};
