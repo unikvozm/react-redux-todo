60ce4bdf6e11cf1fab7774d982e7fa06fad653ca
Natalia Volkova <Natalia.volkofff@gmail.com>
2020-04-23 12:24:41 +0300

    add addTodo form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { Todos } from "./components/Todos";
+import AddTodo from "./components/AddForm";
 
 class App extends React.Component {
   state = {
     todos: [
       { id: 1, content: "buy something" },
-      { id: 1, content: "learn something" },
+      { id: 2, content: "learn something" },
     ],
   };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => todo.id !== id);
+    this.setState({ todos });
+  };
+
+  addTodo = (todo) => {
+    todo.id = this.state.todos.length;
+    const todos = [...this.state.todos, todo];
+    this.setState({ todos });
+  };
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
