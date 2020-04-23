6899a0c1ad517e2b9eeaed1df1ba237d3de636dd
Natalia Volkova <Natalia.volkofff@gmail.com>
2020-04-23 13:44:44 +0300

    recreated the appp with cra + redux template

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import React from "react";
-import { Todos } from "./components/Todos";
-import AddTodo from "./components/AddForm";
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-class App extends React.Component {
-  state = {
-    todos: [
-      { id: 1, content: "buy something" },
-      { id: 2, content: "learn something" },
-    ],
-  };
-
-  deleteTodo = (id) => {
-    const todos = this.state.todos.filter((todo) => todo.id !== id);
-    this.setState({ todos });
-  };
-
-  addTodo = (todo) => {
-    todo.id = this.state.todos.length;
-    const todos = [...this.state.todos, todo];
-    this.setState({ todos });
-  };
-
-  render() {
-    return (
-      <div className="todo-app container">
-        <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-        <AddTodo addTodo={this.addTodo} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
 }
 
 export default App;
